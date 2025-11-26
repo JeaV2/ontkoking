79adbebc58036cbe2ee2bb1f1e37f454e847eb5f
JeaV2 <102710@glr.nl>
2025-11-26 21:28:44 +0100

    Delete admincheck.php

--- admincheck.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION["role"]) || $_SESSION["role"] !== "Admin") {
-    header("Location: ../overzicht.php?error=".urlencode("Toegang geweigerd. U heeft geen toestemming om deze pagina te bekijken."));
-    exit();
-}
